4439db8ead65768ad5ae9e664f6f431ae6d18a47
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-05-30 08:07:09 +0200

    Added the test.

--- features/processes/workspace_run_stdout_capture/_adhesive.py
+++ features/processes/workspace_run_stdout_capture/_adhesive.py
@@ -10,14 +10,4 @@ def stdout_test_local(context):
     assert content == "ABC\n"
 
 
-@adhesive.task("Stdout capture in docker workspace")
-def stdout_test_in_docker(context):
-    with docker.inside(context.workspace, "ubuntu:18.04") as w:
-        content = w.run(
-            "echo ABC",
-            capture_stdout=True)
-
-    assert content == "ABC\n"
-
-
 adhesive.build()
