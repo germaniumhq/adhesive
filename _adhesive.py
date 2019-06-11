3e8638d89dfa45b8680bee19fefa8ec8c2da8744
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-06-11 09:19:47 +0200

    Complex gateway.

--- _adhesive.py
+++ _adhesive.py
@@ -2,52 +2,11 @@ import textwrap
 
 import adhesive
 import gbs
+import ge_tooling
+import ge_git
 from adhesive import scm
 from adhesive.secrets import secret
 from adhesive.workspace import docker
-
-tools = {
-    "mypy": textwrap.dedent("""\
-        FROM germaniumhq/python:3.7
-        ENV REFRESHED_AT 2018.10.14-06:56:31
-        RUN pip install mypy
-        """),
-
-    "ansible": textwrap.dedent("""\
-        FROM germaniumhq/python:3.7
-        ENV REFRESHED_AT 2018.10.14-06:58:16
-        RUN pip install ansible
-    """),
-
-    "flake8": textwrap.dedent("""\
-        FROM germaniumhq/python:3.7
-        ENV REFRESHED_AT 2018.10.14-06:56:31
-        RUN pip install flake8
-    """),
-
-    "python": textwrap.dedent("""\
-        FROM germaniumhq/python:3.7
-    """),
-
-    "behave": textwrap.dedent("""\
-        FROM python:3.7
-
-        RUN pip install behave
-        RUN curl https://get.docker.com | sh
-    """),
-
-    "git": textwrap.dedent("""\
-        FROM germaniumhq/ubuntu:18.04
-        ENV REFRESHED_AT 2018.10.18-05:25:08
-        USER root
-        RUN apt update -y && apt install -y git && rm -rf /var/lib/apt/lists/*
-        USER germanium
-    """),
-
-    "version-manager": textwrap.dedent("""\
-        FROM bmst/version-manager:2.5.0
-    """)
-}
 
 
 @adhesive.task("Read Parameters")
@@ -56,27 +15,19 @@ def read_parameters(context) -> None:
     context.data.test_integration = True
 
 
-@adhesive.task("Checkout Code")
-def checkout_code(context) -> None:
-    scm.checkout(context.workspace)
-
-
 @adhesive.task(r"^Ensure Tooling:\s+(.+)$")
-def ensure_tooling(context, tool_name) -> None:
-    w = context.workspace
-
-    with w.temp_folder():
-        w.write_file("Dockerfile", tools[tool_name])
-        w.run(f"docker build -t germaniumhq/tools-{tool_name}:latest .")
+def gbs_ensure_tooling(context, tool_name) -> None:
+    ge_tooling.ensure_tooling(context, tool_name)
 
 
 @adhesive.task("^Run tool: (.*?)$")
-def run_tool(context, command: str) -> None:
-    tool_name = command.split(" ")[0]
+def gbs_run_tool(context, command: str) -> None:
+    ge_tooling.run_tool(context, command)
 
-    with docker.inside(context.workspace,
-                       f"germaniumhq/tools-{tool_name}") as w:
-        w.run(command)
+
+@adhesive.task("Checkout Code")
+def checkout_code(context) -> None:
+    scm.checkout(context.workspace)
 
 
 @adhesive.task("GBS: lin64")
@@ -118,6 +69,17 @@ def gbs_build_win32(context) -> None:
     #gbs.build(workspace=context.workspace,
     #          platform="python:win32",
     #          gbs_prefix=f"/_gbs/win32/")
+
+
+@adhesive.gateway('Is\ Release\ Version\?')
+def is_release_version(context):
+    current_version = ge_tooling.run_tool(
+        context,
+        "version-manager --tag",
+        capture_stdout=True).strip()
+
+    if ge_git.is_tag_version(current_version):
+        context.data.release_version = True
 
 
 @adhesive.usertask('Publish to PyPI\?')
