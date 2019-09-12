86acec9c124bbcff528eba28e91b8977d3e0bf27
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-09-12 08:00:00 -0700

    All the tests are passing.

--- features/processes/programmatic/adhesive_build_loop_task_after/_adhesive.py
+++ features/processes/programmatic/adhesive_build_loop_task_after/_adhesive.py
@@ -5,18 +5,18 @@ import adhesive
 
 @adhesive.task("Basic loop", loop="data.loop_items")
 def basic_loop(context):
-    if not context.data.executionss:
-        context.data.executionss = set()
+    if not context.data.executions:
+        context.data.executions = set()
 
-    context.data.executionss.add(str(uuid.uuid4()))
+    context.data.executions.add(str(uuid.uuid4()))
 
 
 @adhesive.task("Not a loop")
 def not_a_loop(context):
-    if not context.data.executionss:
-        context.data.executionss = set()
+    if not context.data.executions:
+        context.data.executions = set()
 
-    context.data.executionss.add(str(uuid.uuid4()))
+    context.data.executions.add(str(uuid.uuid4()))
 
 
 result = adhesive.build(initial_data={
