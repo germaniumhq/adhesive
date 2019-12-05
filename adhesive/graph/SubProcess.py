0dedcc2b80f6e09a772e9f9d8c5643786e42852d
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-12-05 06:28:27 +0100

    A single failing mypy check

--- adhesive/graph/SubProcess.py
+++ adhesive/graph/SubProcess.py
@@ -5,17 +5,15 @@ from adhesive.graph.ProcessTask import ProcessTask
 
 class SubProcess(ProcessTask, Process):
     def __init__(self,
-                 *args,
+                 *,
                  parent_process: Process,
                  id: str,
                  name: str):
-        if args:
-            raise Exception("You need to pass arguments by name")
-
         super(SubProcess, self).__init__(
             id=id,
             name=name,
             parent_process=parent_process)
 
     def get_lane_definition(self, task_id: str) -> Lane:
+        assert self.parent_process
         return self.parent_process.get_lane_definition(self.id)
