66c13eb61dd8c8357699a26276383edf02812194
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-08-27 09:55:07 +0200

    Minor arst updates

--- adhesive.sh
+++ adhesive.sh
@@ -4,6 +4,9 @@ echo '##########################################################################
 echo '# adhesive containerized build starting'
 echo '#############################################################################'
 
+# PYTHONUSERSITE disables adding the .local into sys.path, so our $HOME mount
+# doesn't screw up.
+
 docker run -it \
     --rm \
     -v /tmp:/tmp \
@@ -12,9 +15,10 @@ docker run -it \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
     -e HOME=$HOME \
+    -e PYTHONNOUSERSITE=1 \
     -w $(pwd) \
     -u $(id -u):$(id -g) \
     $(id -G | perl -pe 's/(\d+)/--group-add \1/g') \
-    germaniumhq/adhesive:1.3.5 \
+    germaniumhq/adhesive:1.4.14 \
     adhesive $@
 
--- bin/requires_versions.py
+++ bin/requires_versions.py
@@ -5,6 +5,6 @@ with open("requirements.txt", mode="r") as requirements_file:
     for line in requirements_file.readlines():
         line = line.strip()
         if line and not line.startswith("#") and not line.endswith("# optional"):
-            install_requires.append('        "%s"' % line.strip())
+            install_requires.append('        "%s",' % line.strip())
 
-print("\n" + ",\n".join(install_requires))
+print("\n" + "\n".join(install_requires) + "\n    ")
